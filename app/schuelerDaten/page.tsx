aa074d0e0f1a26528fb7631dda2d77682de364ad
seen22 <78369259+seen22@users.noreply.github.com>
2024-10-18 17:31:39 +0200

    erstellt Fahrstunde Ressoucen und mit Schülerdaten verknüpfen

--- app/schuelerDaten/page.tsx
+++ app/schuelerDaten/page.tsx
@@ -57,10 +57,10 @@ export default function HalloPage() {
       const { namedNode, literal } = DataFactory;
       const writer = new Writer();
     
-      // Create a unique subject using timestamp or another identifier
+      // Create a unique studentURI using timestamp or another identifier
       const uniqueID = uuidv4();
-      const uniqueSubject = `https://github.com/seen22/fahrlehre-nextapp/formEntry-${uniqueID}`;
-      const subject = namedNode(uniqueSubject);
+      const uniquestudentURI = `https://github.com/seen22/fahrlehre-nextapp/formEntry-${uniqueID}`;
+      const studentURI = namedNode(uniquestudentURI);
     
       // Get the current timestamp in local time
       const currentTimestamp = new Date().toLocaleString("sv-SE", { timeZone: 'Europe/Berlin' }).replace(' ', 'T') + 'Z';
@@ -68,7 +68,7 @@ export default function HalloPage() {
       // Add the timestamp
       const timestampPredicate = namedNode('fahrl:timestamp');
       const timestampObject = literal(currentTimestamp);
-      writer.addQuad(subject, timestampPredicate, timestampObject);
+      writer.addQuad(studentURI, timestampPredicate, timestampObject);
         
     
       // Map form data to RDF triples
@@ -76,21 +76,17 @@ export default function HalloPage() {
         let predicate, object;
         if (key === 'hasDateOfBirth') {
            predicate = namedNode('fahrl:hasDateOfBirth');
-           object = literal(value as string | number);
-          writer.addQuad(subject, predicate, object);
+           object = literal(value as string | number, namedNode('http://www.w3.org/2001/XMLSchema#date'));
+          writer.addQuad(studentURI, predicate, object);
         } else if (key === 'hasEyewear') {
            predicate = namedNode('fahrl:hasEyewear');
            object =  literal(value as 'true', namedNode('http://www.w3.org/2001/XMLSchema#boolean'));
-          writer.addQuad(subject, predicate, object);
-        } else if (key === 'hasFirstName') {
-          predicate = namedNode('fahrl:hasFirstName');
+          writer.addQuad(studentURI, predicate, object);
+        } else {
+          predicate = namedNode(`fahrl:${key}`);
           object = literal(value as string);
-          writer.addQuad(subject, predicate, object);
-        } else if (key === 'hasLastName') {
-          predicate = namedNode('fahrl:hasLastName');
-          object = literal(value as string);
-          writer.addQuad(subject, predicate, object);
-        }
+          writer.addQuad(studentURI, predicate, object);
+        } 
         
       });
     
@@ -192,347 +188,4 @@ export default function HalloPage() {
   );
 }
 
-         {/* 
-
-        <div>
-          <label className="block text-sm font-medium text-gray-300">Telefonnummer</label>
-          <input
-            type="tel"
-            {...register('phoneNumber', { required: 'Telefonnummer ist erforderlich' })}
-            className="mt-1 block w-full p-2 bg-gray-700 text-white rounded-md"
-          />
-          {getErrorMessage(errors.phoneNumber) && <span className="text-red-500 text-sm">{getErrorMessage(errors.phoneNumber)}</span>}
-        </div> */}
-
-        {/* <div>
-          <label className="block text-sm font-medium text-gray-300">Anschrift</label>
-          <input
-            type="text"
-            {...register('address', { required: 'Anschrift ist erforderlich' })}
-            className="mt-1 block w-full p-2 bg-gray-700 text-white rounded-md"
-          />
-          {getErrorMessage(errors.address) && <span className="text-red-500 text-sm">{getErrorMessage(errors.address)}</span>}
-        </div>
-
-        <div>
-          <label className="block text-sm font-medium text-gray-300">Antrag gestellt am</label>
-          <input
-            type="date"
-            {...register('applicationDate', { required: 'Antrag gestellt am ist erforderlich' })}
-            className="mt-1 block w-full p-2 bg-gray-700 text-white rounded-md"
-          />
-          {getErrorMessage(errors.applicationDate) && <span className="text-red-500 text-sm">{getErrorMessage(errors.applicationDate)}</span>}
-        </div> */}
-
-//...................................................................................................
-//für ID von URL extrahieren 
-//.................................................................................................
-
-// 'use client'
-// import { useForm, FieldError, Merge, FieldErrorsImpl } from 'react-hook-form';
-// import { DataFactory, Writer } from 'n3';
-// import { useState } from 'react';
-// import { v4 as uuidv4 } from 'uuid'; // Falls du UUID verwenden möchtest
-// import { useRouter } from 'next/navigation'; // useRouter für Navigation
-
-// export default function HalloPage() {
-//   const { register, handleSubmit, formState: { errors }, reset } = useForm();
-//   const [rdfData, setRdfData] = useState('');
-//   const router = useRouter(); // Für die Navigation
-//   const [formData, setFormData] = useState({}); 
-//   // Funktion zum Senden der Daten an den Fuseki-Server
-//   const sendToFuseki = async (rdfData: any) => {
-//     const updateQuery = `
-//       PREFIX ex: <https://github.com/seen22/fahrlere-nextapp/>
-//       PREFIX schema: <http://schema.org/>
-//       PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-      
-//       INSERT DATA {
-//         ${rdfData}
-//       }
-//     `;
-//     try {
-//       const response = await fetch('http://localhost:3030/test_next/update', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/sparql-update',
-//         },
-//         body: updateQuery,
-//       });
-  
-//       const responseText = await response.text();
-//       if (response.ok) {
-//         console.log('Daten erfolgreich an Fuseki gesendet');
-//       } else {
-//         console.error('Fehler beim Senden der Daten an Fuseki', responseText);
-//       }
-//     } catch (error) {
-//       console.error('Fehler:', error);
-//     }
-//   };
-
-//   // Funktion zur Generierung einer eindeutigen Schüler-ID und zum Speichern
-//   const onSubmit = async (formData: any) => {
-//     const { namedNode, literal } = DataFactory;
-//     const writer = new Writer();
-
-//     // Eindeutige ID für den Schüler erzeugen (z.B. mit uuidv4 oder Date.now)
-//     const studentID = uuidv4(); // Falls du UUID verwenden möchtest: uuidv4()
-//     console.log('Generated Student ID:', studentID);
-
-//     // Subject mit dieser eindeutigen ID
-//     const subject = namedNode(`https://github.com/seen22/fahrlere-nextapp/student-${studentID}`);
-
-//     // Aktuelles Datum (Timestamp)
-//     const currentTimestamp = new Date().toISOString();
-
-//     // Füge Timestamp zu RDF hinzu
-//     const timestampPredicate = namedNode('https://github.com/seen22/fahrlere-nextapp/timestamp');
-//     const timestampObject = literal(currentTimestamp, namedNode('http://www.w3.org/2001/XMLSchema#dateTime'));
-//     writer.addQuad(subject, timestampPredicate, timestampObject);
-
-//     // Map Form Data to RDF triples
-//     Object.entries(formData).forEach(([key, value]) => {
-//       let predicate;
-//       let object;
-
-//       if (key === 'birthDate') {
-//         predicate = namedNode('https://github.com/seen22/fahrlere-nextapp/birthDate');
-//         object = literal(value as string | number, namedNode('http://www.w3.org/2001/XMLSchema#date'));
-//       } else if (key === 'eyewear') {
-//         predicate = namedNode('http://schema.org/eyewear');
-//         object = literal(value as string | number);
-//       } else {
-//         predicate = namedNode(`https://github.com/seen22/fahrlere-nextapp/${key}`);
-//         object = literal(value as string | number);
-//       }
-//       writer.addQuad(subject, predicate, object);
-//     });
-
-//     // Serialize and send to Fuseki
-//     writer.end((error: any, result: any) => {
-//       if (error) {
-//         console.error('Failed to serialize RDF data', error);
-//       } else {
-//         console.log('RDF-Triple:\n', result);
-//         setRdfData(result);
-//         sendToFuseki(result); // Send the serialized data to Fuseki
-
-//         // Weiterleitung zur "users"-Seite mit der generierten studentID in der URL
-//         router.push(`/users?id=${studentID}`);
-//       }
-//     });
-//   };
-
-//   const handleChange = (event: { target: { name: string; value: string; }; }) => {
-//     setFormData({
-//       ...formData, // Use the existing formData state
-//       [event.target.name]: event.target.value, // Update the specific field
-//     });
-//   };
-  
-  
-//     const getErrorMessage = (error: FieldError | Merge<FieldError, FieldErrorsImpl<any>> | undefined): string | undefined => {
-//     if (error && 'message' in error) {
-//       return error.message as string;
-//     }
-//     return undefined;
-//   };
-
-//   const fetchFromFuseki= async() => {
-//     throw new Error('Function not implemented.');
-//   }
-
-//     return (
-//     <div className="max-w-lg mx-auto p-6 bg-gray-800 rounded-lg">
-//       <h1 className="text-4xl font-bold mb-6 text-white">Schülerdaten Formular</h1>
-//       <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-//         <div>
-//           <label className="block text-sm font-medium text-gray-300">Vorname</label>
-//           <input
-//             type="text"
-//             {...register('firstName', { required: 'Vorname ist erforderlich' })}
-//             className="mt-1 block w-full p-2 bg-gray-700 text-white rounded-md"
-//             onChange={handleChange}
-//             name="firstName" 
-//           />
-//           {getErrorMessage(errors.firstName) && <span className="text-red-500 text-sm">{getErrorMessage(errors.firstName)}</span>}
-//         </div>
-
-//         <div>
-//           <label className="block text-sm font-medium text-gray-300">Nachname</label>
-//           <input
-//             type="text"
-//             {...register('lastName', { required: 'Nachname ist erforderlich' })}
-//             className="mt-1 block w-full p-2 bg-gray-700 text-white rounded-md"
-//             onChange={handleChange}
-//             name="lastName"
-//           />
-//           {getErrorMessage(errors.lastName) && <span className="text-red-500 text-sm">{getErrorMessage(errors.lastName)}</span>}
-//         </div>
-
-//         <div>
-//           <label className="block text-sm font-medium text-gray-300">Geburtsdatum</label>
-//           <input
-//             type="date"
-//             {...register('birthDate', { required: 'Geburtsdatum ist erforderlich' })}
-//             className="mt-1 block w-full p-2 bg-gray-700 text-white rounded-md"
-//             onChange={handleChange}
-//             name="birthDate"
-//           />
-//           {getErrorMessage(errors.birthDate) && <span className="text-red-500 text-sm">{getErrorMessage(errors.birthDate)}</span>}
-//         </div>
-
-//         <div className="flex items-center">
-//           <input
-//             type="checkbox"
-//             {...register('eyewear')}
-//             className="h-4 w-4 text-blue-600 bg-gray-700 border-gray-600 rounded focus:ring-blue-500"
-//             onChange={handleChange}
-//             name="eyewear"
-//           /> 
-//           <label className="ml-2 text-sm font-medium text-gray-300">Sehhilfe</label>
-//         </div> 
-
-//         <button
-//           type="submit"
-//           className="w-full bg-blue-600 text-white p-2 rounded-md hover:bg-blue-700 transition"
-//         >
-//           Speichern
-//         </button>
-
-//         <button
-//           type="button"
-//           className="w-full bg-blue-600 text-white p-2 rounded-md hover:bg-blue-700 transition"
-//           onClick={fetchFromFuseki}
-//         >
-//           Download
-//         </button>
-//       </form>
-//     </div>
-//   );
-// }
-
-
-
-// 'use client'
-// import { useForm, FieldError, Merge, FieldErrorsImpl } from 'react-hook-form';
-
-// export default function schuelerDatenPage() {
-//   const { register, handleSubmit, formState: { errors }, reset } = useForm();
-
-//   const onSubmit = async (studentData: any) => {
-//     try{  
-//     // Send data to the API
-//     const response = await fetch('/api/saveStudentData', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify(studentData),
-//     });   
-//     const result = await response.json();
-//     console.log('API Response:', result);
-
-//     if (response.ok && result.id) {
-//       console.log(' Saved successfully');
-//       window.location.href = `/DashboardPage?id=${result.id}`;
-//       //reset();  
-//     } else {
-//       console.error('Failed to save data', result.message);
-//     }
-//   } catch (error) {
-//     console.error('Error saving data:', error);
-//   }
-// };
-
-//   const getErrorMessage = (error: FieldError | Merge<FieldError, FieldErrorsImpl<any>> | undefined): string | undefined => {
-//     if (error && 'message' in error) {
-//       return error.message as string;
-//     }
-//     return undefined;
-//   };
-
-//   return (
-//     <div className="max-w-lg mx-auto p-6 bg-gray-800 rounded-lg">
-//       <h1 className="text-4xl font-bold mb-6 text-white">Schülerdaten Formular</h1>
-//       <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-//         <div>
-//           <label className="block text-sm font-medium text-gray-300">Vorname</label>
-//           <input
-//             type="text"
-//             {...register('firstName', { required: 'Vorname ist erforderlich' })}
-//             className="mt-1 block w-full p-2 bg-gray-700 text-white rounded-md"
-//           />
-//           {getErrorMessage(errors.firstName) && <span className="text-red-500 text-sm">{getErrorMessage(errors.firstName)}</span>}
-//         </div>
-
-//          <div>
-//           <label className="block text-sm font-medium text-gray-300">Nachname</label>
-//           <input
-//             type="text"
-//             {...register('lastName', { required: 'Nachname ist erforderlich' })}
-//             className="mt-1 block w-full p-2 bg-gray-700 text-white rounded-md"
-//           />
-//           {getErrorMessage(errors.lastName) && <span className="text-red-500 text-sm">{getErrorMessage(errors.lastName)}</span>}
-//         </div>
-
-//         <div>
-//           <label className="block text-sm font-medium text-gray-300">Geburtsdatum</label>
-//           <input
-//             type="date"
-//             {...register('birthDate', { required: 'Geburtsdatum ist erforderlich' })}
-//             className="mt-1 block w-full p-2 bg-gray-700 text-white rounded-md"
-//           />
-//           {getErrorMessage(errors.birthDate) && <span className="text-red-500 text-sm">{getErrorMessage(errors.birthDate)}</span>}
-//         </div>
-
-//         <div>
-//           <label className="block text-sm font-medium text-gray-300">Telefonnummer</label>
-//           <input
-//             type="tel"
-//             {...register('phoneNumber', { required: 'Telefonnummer ist erforderlich' })}
-//             className="mt-1 block w-full p-2 bg-gray-700 text-white rounded-md"
-//           />
-//           {getErrorMessage(errors.phoneNumber) && <span className="text-red-500 text-sm">{getErrorMessage(errors.phoneNumber)}</span>}
-//         </div>
-
-//         <div>
-//           <label className="block text-sm font-medium text-gray-300">Anschrift</label>
-//           <input
-//             type="text"
-//             {...register('address', { required: 'Anschrift ist erforderlich' })}
-//             className="mt-1 block w-full p-2 bg-gray-700 text-white rounded-md"
-//           />
-//           {getErrorMessage(errors.address) && <span className="text-red-500 text-sm">{getErrorMessage(errors.address)}</span>}
-//         </div>
-
-//         <div>
-//           <label className="block text-sm font-medium text-gray-300">Antrag gestellt am</label>
-//           <input
-//             type="date"
-//             {...register('applicationDate', { required: 'Antrag gestellt am ist erforderlich' })}
-//             className="mt-1 block w-full p-2 bg-gray-700 text-white rounded-md"
-//           />
-//           {getErrorMessage(errors.applicationDate) && <span className="text-red-500 text-sm">{getErrorMessage(errors.applicationDate)}</span>}
-//         </div>
-
-//         <div className="flex items-center">
-//           <input
-//             type="checkbox"
-//             {...register('sehhilfe')}
-//             className="h-4 w-4 text-blue-600 bg-gray-700 border-gray-600 rounded focus:ring-blue-500"
-//           /> 
-//           <label className="ml-2 text-sm font-medium text-gray-300">Sehhilfe</label>
-//         </div>
-
-//         <button
-//           type="submit"
-//           className="w-full bg-blue-600 text-white p-2 rounded-md hover:bg-blue-700 transition"
-//         >
-//           Speichern
-//         </button>
-//       </form>
-//     </div>
-//   );
-// }
+        
--- app/sonderfahrt/page.tsx
+++ app/sonderfahrt/page.tsx
@@ -2,6 +2,7 @@
 import { useForm } from 'react-hook-form';
 import React, { useEffect, useState } from 'react';
 import BackButton from '../BackButton/page';
+import { DataFactory, Writer } from 'n3';
 
 const Sonderfahrt = () => {
   const { register, handleSubmit } = useForm();
@@ -26,79 +27,91 @@ const Sonderfahrt = () => {
     }
 
     try {
-      // Construct the RDF data for the checkboxes
-      const triples = Object.entries(checkboxData).map(([key, value]) => {
-        const predicate = `fahrl:${key}`;
-        const object = value ? `"true"` : `"false"`;
-        return `<https://github.com/seen22/fahrlehre-nextapp/formEntry-${studentId}> <${predicate}> ${object} .`;
-      }).join('\n');
+      const { namedNode, literal } = DataFactory;
+      const writer = new Writer();
+      const studentURI = `https://github.com/seen22/fahrlehre-nextapp/formEntry-${studentId}`;
+      const lessonURI = `https://github.com/seen22/fahrlehre-nextapp/lesson-${studentId}`;
 
-      const updateQuery = `
-       PREFIX fahrl: <https://github.com/seen22/fahrlehre-nextapp/vocabulary.rdf#>
-          PREFIX schema: <http://schema.org/>
-          PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-          PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-          PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-          PREFIX owl: <http://www.w3.org/2002/07/owl#>
-        INSERT DATA {
-          ${triples}
-        }
-      `;
 
-      // Send the update query to Fuseki
-      const response = await fetch('http://localhost:3030/FahrlehrerApp/update', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/sparql-update',
-        },
-        body: updateQuery,
+      Object.entries(checkboxData).forEach(([key, value]) => {
+        const predicate = namedNode(`fahrl:${key}`);
+        const object = literal(value as 'true', namedNode('http://www.w3.org/2001/XMLSchema#boolean'));
+        writer.addQuad(namedNode(lessonURI), predicate, object);
       });
 
-      const result = await response.text();
-      if (response.ok) {
-        console.log('Checkbox data saved successfully');
-        
-        console.log('student ID ', studentId);
-        console.log('RDF', triples);
-        console.log('RDF2', updateQuery);
-        window.location.href = `/DashboardPage?id=${studentId}`;
 
-      } else {
-        console.error('Failed to save checkbox data:', result);
-      }
+      writer.addQuad(
+        namedNode(studentURI),
+        namedNode('fahrl:hasDrivingLesson'),
+        namedNode(lessonURI)
+      );
+
+      // Serialisiere und sende die Daten an Fuseki
+      writer.end(async (error, result) => {
+        if (error) {
+          console.error('Error while serializing RDF:', error);
+          return;
+        }
+
+        const updateQuery = `
+          PREFIX fahrl: <https://github.com/seen22/fahrlehre-nextapp/vocabulary.rdf#>
+          PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+          INSERT DATA {
+            ${result}
+          }
+        `;
+
+        const response = await fetch('http://localhost:3030/FahrlehrerApp/update', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/sparql-update',
+          },
+          body: updateQuery,
+        });
+
+        if (response.ok) {
+          console.log('Sonderfahrt-Daten erfolgreich gespeichert.');
+          window.location.href = `/DashboardPage?id=${studentId}`;
+        } else {
+          const resultText = await response.text();
+          console.error('Fehler beim Speichern der Sonderfahrt-Daten:', resultText);
+        }
+      });
     } catch (error) {
-      console.error('Error saving checkbox data:', error);
+      console.error('Fehler beim Speichern der Sonderfahrt-Daten:', error);
     }
   };
 
   return (
     <div className="max-w-xl mx-auto p-4">
-      <BackButton/>
+      <BackButton />
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
         <div>
-          <h3 className="font-bold text-xl mb-2">Sonderfahrt</h3> 
+          <h3 className="font-bold text-xl mb-2">Sonderfahrt</h3>
           <div className="flex justify-between items-center">
-            <label className="text-lg font-medium">Überlandsfahrt</label> 
-            <input type="checkbox" {...register('CountryRoadDrives')} className="h-5 w-5" /> 
+            <label className="text-lg font-medium">Überlandsfahrt</label>
+            <input type="checkbox" {...register('countryRoadDrive')} className="h-5 w-5" />
           </div>
           <div className="flex justify-between items-center">
-            <label className="text-lg font-medium">Autobahn</label> 
-            <input type="checkbox" {...register('HighwayDrives')} className="h-5 w-5" />
+            <label className="text-lg font-medium">Autobahn</label>
+            <input type="checkbox" {...register('highwayDrive')} className="h-5 w-5" />
           </div>
           <div className="flex justify-between items-center">
-            <label className="text-lg font-medium">Nachtsfahrt</label> 
-            <input type="checkbox" {...register('NightDrive')} className="h-5 w-5" />
+            <label className="text-lg font-medium">Nachtsfahrt</label>
+            <input type="checkbox" {...register('nightDrive')} className="h-5 w-5" />
           </div>
         </div>
-  
+
         <button type="submit" className="w-full bg-blue-600 text-white p-3 rounded-md hover:bg-blue-700 transition">
           Speichern
         </button>
       </form>
     </div>
-  );  
+  );
 };
 
 export default Sonderfahrt;
+
+
 
 
